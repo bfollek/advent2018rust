d71a32d994f4c5f0439e085ca966572eada281ff
Bob Follek <bfollek@gmail.com>
2019-04-26 22:02:31 -0400

    Added part1_fold

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -11,3 +11,9 @@ pub fn part1(file_name: &str) -> Result<i32, Box<Error>> {
   }
   Ok(res)
 }
+
+pub fn part1_fold(file_name: &str) -> Result<i32, Box<Error>> {
+  let v = util::text_file_to_vector(file_name)?;
+  let res: i32 = v.iter().fold(0, |sum, s| sum + s.parse::<i32>().unwrap());
+  Ok(res)
+}
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<Error>> {
     println!(
-        "Day 01, par 1, should be 592: {}",
-        day01::part1("testdata/day01.txt")?
+        "Day 01, par 1, should be 592: {} {}",
+        day01::part1("testdata/day01.txt")?,
+        day01::part1_fold("testdata/day01.txt")?
     );
     Ok(())
 }
