dce3767613b8fc7e46400e1b0cdf4b9e0b4fb90e
Bob Follek <bfollek@gmail.com>
2019-04-27 15:15:42 -0400

    WIP

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -14,6 +14,9 @@ pub fn part1(file_name: &str) -> Result<i32, Box<Error>> {
 
 pub fn part1_fold(file_name: &str) -> Result<i32, Box<Error>> {
   let v = util::text_file_to_vector(file_name)?;
+  // The function passed to fold returns an i32, not a Result, so
+  // I can't use the ? operator on s.parse(). Instead, unwrap()
+  // works, but it panics on Error instead of returning it.
   let res: i32 = v.iter().fold(0, |sum, s| sum + s.parse::<i32>().unwrap());
   Ok(res)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 mod day01;
 mod util;
 
-use std::error::Error;
-
-fn main() -> Result<(), Box<Error>> {
-    println!(
-        "Day 01, par 1, should be 592: {} {}",
-        day01::part1("testdata/day01.txt")?,
-        day01::part1_fold("testdata/day01.txt")?
-    );
-    Ok(())
+fn main() {
+    let mut expected = 592;
+    assert_eq!(expected, day01::part1("testdata/day01.txt").unwrap());
+    assert_eq!(expected, day01::part1_fold("testdata/day01.txt").unwrap());
 }
