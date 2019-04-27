cea6dad924494f471771a953793a520b77d2dcf4
Bob Follek <bfollek@gmail.com>
2019-04-27 15:39:41 -0400

    WIP

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -9,3 +9,14 @@ pub fn text_file_to_vector(file_name: &str) -> Result<Vec<String>, Box<Error>> {
   let res = txt.split("\n").map(|s| s.to_string()).collect();
   Ok(res)
 }
+
+pub fn text_file_to_vector_i32(file_name: &str) -> Result<Vec<i32>, Box<Error>> {
+  let mut f = fs::File::open(file_name)?;
+  let mut txt = String::new();
+  f.read_to_string(&mut txt)?;
+  let res = txt
+    .split("\n")
+    .map(|s| s.to_string().parse::<i32>().unwrap())
+    .collect();
+  Ok(res)
+}
