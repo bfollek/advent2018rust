5ffeb3868743fc23d229172f696bd9bfec3c6192
Bob Follek <bfollek@gmail.com>
2019-05-01 20:17:37 -0400

    day_02.part_1 working

--- src/day_02/mod.rs
+++ src/day_02/mod.rs
@@ -1,4 +1,5 @@
 use crate::util;
+use std::collections::HashMap;
 use std::error::Error;
 
 // part_1 does a checksum on the box IDs.
@@ -6,22 +7,35 @@ pub fn part_1(file_name: &str) -> Result<i32, Box<Error>> {
   let mut cnt_2 = 0;
   let mut cnt_3 = 0;
   let ids = util::text_file_to_vector(file_name)?;
-  for id in ids {}
-  Ok(0)
+  for id in ids {
+    let (has_2, has_3) = check_id(id);
+    if has_2 {
+      cnt_2 += 1;
+    }
+    if has_3 {
+      cnt_3 += 1;
+    }
+  }
+  Ok(cnt_2 * cnt_3)
 }
 
-// func Part1(fileName string) int {
-// 	cnt2 := 0
-// 	cnt3 := 0
-// 	ids := util.MustLoadStringSlice(fileName)
-// 	for _, id := range ids {
-// 		has2, has3 := checkID(id)
-// 		if has2 {
-// 			cnt2++
-// 		}
-// 		if has3 {
-// 			cnt3++
-// 		}
-// 	}
-// 	return cnt2 * cnt3
-// }
+// check_id checks IDs for letters that appear exactly 2 or 3 times.
+fn check_id(id: String) -> (bool, bool) {
+  let mut char_map = HashMap::new();
+  for c in id.chars() {
+    let cnt = char_map.entry(c).or_insert(0);
+    *cnt += 1;
+  }
+  let (mut has_2, mut has_3) = (false, false);
+  for cnt in char_map.values() {
+    match cnt {
+      2 => has_2 = true,
+      3 => has_3 = true,
+      _ => (),
+    }
+    if has_2 && has_3 {
+      break; // No need to keep checking
+    }
+  }
+  (has_2, has_3)
+}
