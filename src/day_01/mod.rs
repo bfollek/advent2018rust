2483a461e5f08877589a685f6b822448cff98896
Bob Follek <bfollek@gmail.com>
2019-05-03 18:40:24 -0400

    WIP

--- src/day_01/mod.rs
+++ src/day_01/mod.rs
@@ -4,26 +4,22 @@ use std::error::Error;
 
 // part_1 sums the frequency changes.
 pub fn part_1(file_name: &str) -> Result<i32, Box<Error>> {
-  let v = util::text_file_to_vector(file_name)?;
+  let v: Vec<i32> = util::parse_text_file_to_vector(file_name)?;
   let mut res = 0;
-  for s in v {
-    let i: i32 = s.parse()?;
+  for i in v {
     res += i;
   }
   Ok(res)
 }
 
 pub fn part_1_fold(file_name: &str) -> Result<i32, Box<Error>> {
-  let v = util::text_file_to_vector(file_name)?;
-  // The function passed to fold returns an i32, not a Result, so
-  // I can't use the ? operator on s.parse(). Instead, unwrap()
-  // works, but it panics on Error instead of returning it.
-  let res: i32 = v.iter().fold(0, |sum, s| sum + s.parse::<i32>().unwrap());
+  let v = util::parse_text_file_to_vector(file_name)?;
+  let res = v.iter().fold(0, |sum, i| sum + i);
   Ok(res)
 }
 
 pub fn part_1_sum(file_name: &str) -> Result<i32, Box<Error>> {
-  let v = util::text_file_to_vector_i32(file_name)?;
+  let v = util::parse_text_file_to_vector(file_name)?;
   let res = v.iter().sum();
   Ok(res)
 }
@@ -32,7 +28,7 @@ pub fn part_1_sum(file_name: &str) -> Result<i32, Box<Error>> {
 pub fn part_2(file_name: &str) -> Result<i32, Box<Error>> {
   let mut freq = 0;
   let mut seen = HashMap::new();
-  let v = util::text_file_to_vector_i32(file_name)?;
+  let v = util::parse_text_file_to_vector(file_name)?;
   loop {
     // We need &v to avoid a borrow checking error.
     // http://xion.io/post/code/rust-for-loop.html
