227dcd6137866da7e06505cd32ff8e8488e0883a
Bob Follek <bfollek@gmail.com>
2019-05-04 08:40:56 -0400

    modified:   src/util/mod.rs Removed unwrap() for consistent error handling.

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs;
 use std::io::prelude::*;
+use std::marker::Sized;
 
 pub fn text_file_to_vector<T: Parses>(file_name: &str) -> Result<Vec<T>, Box<Error>> {
   let mut f = fs::File::open(file_name)?;
@@ -9,24 +10,29 @@ pub fn text_file_to_vector<T: Parses>(file_name: &str) -> Result<Vec<T>, Box<Err
   let mut v: Vec<T> = Vec::new();
   let lines = txt.split("\n");
   for l in lines {
-    let t = T::parse(l);
+    let t = T::parse(l)?;
     v.push(t);
   }
   Ok(v)
 }
 
 pub trait Parses {
-  fn parse(raw: &str) -> Self;
+  fn parse(raw: &str) -> Result<Self, Box<Error>>
+  where
+    Self: Sized;
 }
 
 impl Parses for i32 {
-  fn parse(raw: &str) -> Self {
-    raw.to_string().parse().unwrap()
+  fn parse(raw: &str) -> Result<Self, Box<Error>> {
+    raw
+      .to_string()
+      .parse()
+      .map_err(|e: std::num::ParseIntError| e.into()) // Converts to Box
   }
 }
 
 impl Parses for String {
-  fn parse(raw: &str) -> Self {
-    raw.to_string()
+  fn parse(raw: &str) -> Result<Self, Box<Error>> {
+    Ok(raw.to_string())
   }
 }
