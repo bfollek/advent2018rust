193c7f5f32bf462504277a3064fa31098458ac36
Bob Follek <bfollek@gmail.com>
2019-05-04 08:48:39 -0400

    modified:   src/util/mod.rs Rename trait/impl to avoid "parse" confusion

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,36 +3,36 @@ use std::fs;
 use std::io::prelude::*;
 use std::marker::Sized;
 
-pub fn text_file_to_vector<T: Parses>(file_name: &str) -> Result<Vec<T>, Box<Error>> {
+pub fn text_file_to_vector<T: ConvertsToText>(file_name: &str) -> Result<Vec<T>, Box<Error>> {
   let mut f = fs::File::open(file_name)?;
   let mut txt = String::new();
   f.read_to_string(&mut txt)?;
   let mut v: Vec<T> = Vec::new();
   let lines = txt.split("\n");
   for l in lines {
-    let t = T::parse(l)?;
+    let t = T::convert_to_text(l)?;
     v.push(t);
   }
   Ok(v)
 }
 
-pub trait Parses {
-  fn parse(raw: &str) -> Result<Self, Box<Error>>
+pub trait ConvertsToText {
+  fn convert_to_text(raw: &str) -> Result<Self, Box<Error>>
   where
     Self: Sized;
 }
 
-impl Parses for i32 {
-  fn parse(raw: &str) -> Result<Self, Box<Error>> {
+impl ConvertsToText for i32 {
+  fn convert_to_text(raw: &str) -> Result<Self, Box<Error>> {
     raw
       .to_string()
       .parse()
-      .map_err(|e: std::num::ParseIntError| e.into()) // Converts to Box
+      .map_err(|e: std::num::ParseIntError| e.into()) // Box the error
   }
 }
 
-impl Parses for String {
-  fn parse(raw: &str) -> Result<Self, Box<Error>> {
+impl ConvertsToText for String {
+  fn convert_to_text(raw: &str) -> Result<Self, Box<Error>> {
     Ok(raw.to_string())
   }
 }
